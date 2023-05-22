0f63f32657dfa4ef67c0d4ee17f8461cf28222bb
Ilya Lebedev <melevir@gmail.com>
2023-05-22 19:46:50 +0400

    fix typo in sentiment check

--- functions/level_1_5/four_sentiment.py
+++ functions/level_1_5/four_sentiment.py
@@ -4,8 +4,8 @@ def check_tweet_sentiment(text: str, good_words: set[str], bad_words: set[str]) 
     words = [w.lower() for w in text.split()]
     good_words_num = sum(1 for w in words if w in good_words)
     bad_words_num = sum(1 for w in words if w in bad_words)
-    if not good_words_num and not bad_words_num or good_words == bad_words:
+    if not good_words_num and not bad_words_num or good_words_num == bad_words_num:
         return None
-    if good_words > bad_words:
+    if good_words_num > bad_words_num:
         return "GOOD"
     return "BAD"
