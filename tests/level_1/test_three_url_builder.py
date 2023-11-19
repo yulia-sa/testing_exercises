3b56b3697f3f27317e07468291ad7a1387c44474
Yulia S <sav.jul@gmail.com>
2023-11-19 19:57:42 +0300

    Test three from level_1

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,20 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url("https://habr.com",
+                     "ru/search/") == "https://habr.com/ru/search/"
+    assert build_url("https://habr.com",
+                     "ru/search/",
+                     {}) == "https://habr.com/ru/search/"
+    assert build_url("https://habr.com",
+                     "ru/search/",
+                     {
+                         "q": "2023",
+                     }) == "https://habr.com/ru/search/?q=2023"
+    assert build_url("https://habr.com",
+                     "ru/search/",
+                     {
+                         "q": "python",
+                         "target_type": "posts",
+                         "order": "relevance"
+                     }) == "https://habr.com/ru/search/?q=python&target_type=posts&order=relevance"
