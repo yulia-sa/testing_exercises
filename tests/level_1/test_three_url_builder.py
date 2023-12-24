48c20ee050cc3a4c22d9270a1b8dcc819f84e1cd
Yulia S <sav.jul@gmail.com>
2023-12-25 02:19:49 +0300

    Parametrize test three from level_1

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,30 +1,24 @@
+import pytest
 from functions.level_1.three_url_builder import build_url
 
 
-def test_build_url_without_get_params():
-    assert build_url("https://habr.com",
-                     "ru/search/") == "https://habr.com/ru/search/"
-
-
-def test_build_url_with_empty_get_params():
-    assert build_url("https://habr.com",
-                     "ru/search/",
-                     {}) == "https://habr.com/ru/search/"
-
-
-def test_build_url_with_one_param_in_get_params():
-    assert build_url("https://habr.com",
-                     "ru/search/",
-                     {
-                         "q": "2023",
-                     }) == "https://habr.com/ru/search/?q=2023"
-
-
-def test_build_url_with_several_params_in_get_params():
-    assert build_url("https://habr.com",
-                     "ru/search/",
-                     {
-                         "q": "python",
-                         "target_type": "posts",
-                         "order": "relevance"
-                     }) == "https://habr.com/ru/search/?q=python&target_type=posts&order=relevance"
+@pytest.mark.parametrize(
+    "host_name,relative_url,get_params,expected_result_url",
+    [
+        ("https://habr.com", "ru/search/", None, "https://habr.com/ru/search/"),
+        ("https://habr.com", "ru/search/", {}, "https://habr.com/ru/search/"),
+        ("https://habr.com", "ru/search/", {"q": "2023"}, "https://habr.com/ru/search/?q=2023"),
+        (
+            "https://habr.com",
+            "ru/search/",
+            {
+                "q": "python",
+                "target_type": "posts",
+                "order": "relevance"
+            },
+            "https://habr.com/ru/search/?q=python&target_type=posts&order=relevance"
+        )
+    ]
+)
+def test__build_url(host_name, relative_url, get_params, expected_result_url):
+    assert build_url(host_name, relative_url, get_params) == expected_result_url
